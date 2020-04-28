62c943a05a985dc744a966d2f423b0022eabb432
Atul Verma <44652100+Atulbargotra@users.noreply.github.com>
2020-04-29 03:03:44 +0530

    Update theme.js

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -3,3 +3,21 @@ $('.datepicker').each(function(){
 		field: this
 	});
 });
+
+
+function mode() {
+	if (document.getElementById('mode').className === 'light-mode'){
+		document.getElementById('mode').className = 'dark-mode';
+		document.getElementById('card0').className = 'dark-mode';
+		document.getElementById('card1').className = 'dark-mode';
+		document.getElementById('card2').className = 'dark-mode';
+		document.getElementById('card2').className = 'dark-mode';
+
+	}
+	else {
+		document.getElementById('mode').className = 'light-mode';
+		document.getElementById('card0').className = 'light-mode';
+		document.getElementById('card1').className = 'light-mode';
+		document.getElementById('card2').className = 'light-mode';
+	}
+}
