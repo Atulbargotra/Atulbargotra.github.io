3ef7405a127f96c2bd6e7244e839d67973547ef6
Atul <atul.bargotra@outlook.com>
2020-05-03 13:57:57 +0530

    ui changed

---
+++ assets/js/global.js
@@ -0,0 +1,1 @@
+var web_mode = 'light-mode';
--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,32 +1,62 @@
-$('.datepicker').each(function(){
+$('.datepicker').each(function () {
 	var picker = new Pikaday({
 		field: this
 	});
 });
+// window.web_mode = 'light-mode'
+// function light_mode() {
+// 	document.getElementById('mode').className = 'light-mode';
+// 	try {
+// 		var y = document.getElementsByClassName("icon-light");
+// 		y[0].style.color = 'rgb(21,21,21)';
+// 		y[1].style.color = 'rgb(21,21,21)';
+// 		y[2].style.color = 'rgb(21,21,21)';
+// 	}
+// 	catch (err) {
 
+// 	}
+// 	finally {
+// 		web_mode = 'light-mode';
+// 		var y = document.getElementsByClassName("card-body");
+// 		for (var i = 0; i < y.length; i++) {
+// 			y[i].style.backgroundColor = "white";
+// 		}
+// 	}
+// }
+// function dark_mode() {
+// 	document.getElementById('mode').className = 'dark-mode';
+// 	try {
+// 		var x = document.getElementsByClassName("icon-light");
+// 		x[0].style.color = 'white';
+// 		x[1].style.color = 'white';
+// 		x[2].style.color = 'white';
+// 	}
+// 	catch (err) {
+// 	}
+// 	finally {
+// 		web_mode = 'dark-mode';
+// 		var y = document.getElementsByClassName("card-body");
+// 		console.log(y.length)
+// 		for (var i = 0; i < y.length; i++) {
+// 			y[i].style.backgroundColor = "rgb(21,21,21)";
+// 		}
+// 	}
+// }
+// function mode() {
+// 	if (web_mode === 'light-mode') {
+// 		dark_mode();
+// 	}
 
-function mode() {
-	
-	if (document.getElementById('mode').className === 'light-mode'){
-		document.getElementById('mode').className = 'dark-mode';
-		document.getElementById('card0').className = 'dark-mode';
-		document.getElementById('card1').className = 'dark-mode';
-		document.getElementById('card2').className = 'dark-mode';
-		document.getElementById('card2').className = 'dark-mode';
-		var x = document.getElementsByClassName("icon-light");
-		x[0].style.color = 'white';
-		x[1].style.color = 'white';
-		x[2].style.color = 'white';
-	}
-	
-	else {
-		document.getElementById('mode').className = 'light-mode';
-		document.getElementById('card0').className = 'light-mode';
-		document.getElementById('card1').className = 'light-mode';
-		document.getElementById('card2').className = 'light-mode';
-		var y = document.getElementsByClassName("icon-light");
-		y[0].style.color = 'black';
-		y[1].style.color = 'black';
-		y[2].style.color = 'black';
-	}
-}
+// 	else {
+// 		light_mode();
+// 	}
+// }
+// function mode2() {
+// 	console.log(web_mode);
+// 	if (web_mode === 'light-mode') {
+// 		light_mode()
+// 	}
+// 	else {
+// 		dark_mode()
+// 	}
+// }
